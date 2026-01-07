1716df97b5871acebedc623ef18d4de72beec5a7
jeonnni <ymymda0711@gmail.com>
2026-01-07 22:01:35 +0900

    Cleanup

--- src/App.js
+++ src/App.js
@@ -2,26 +2,22 @@
 // import title from "./App.module.css"
 import { useEffect, useState } from "react";
 
-// function Hello(){
-//   useEffect(()=>{
-//     // component가 처음 생성될 때 
-//     console.log("created");
-//     return () => console.log("destroyed")
-//   },[])
-//   return <h1>Hello</h1>
-// }
-function Hello() {
-  function byFn(){
-    console.log("destroyed");
-  }
-  function hiFn(){
-    console.log("created");
-    return byFn;
-  }
-  useEffect(hiFn, []);
-
+function Hello(){
+  // component가 처음 생성될 때
+  useEffect(() => {
+    console.log("hi");
+    return ()=>{console.log("bye");
+    }
+  }, []);
+  useEffect(() => {
+    console.log("hi");
+    return function(){
+      console.log("bye");
+    }
+  }, []);
   return <h1>Hello</h1>;
 }
+
 
 function App() {
   const [showing, setShowing] = useState(false);
